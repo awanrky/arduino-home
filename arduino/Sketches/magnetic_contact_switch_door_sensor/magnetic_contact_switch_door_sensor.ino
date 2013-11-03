1abb3f76c855dbbde5da0028498545dc20c42733
Ott, Mark <mark.ott@pearson.com>
2013-11-03 06:59:44 -0800

    sketches for expirimenting with sensors

---
+++ arduino/Sketches/magnetic_contact_switch_door_sensor/magnetic_contact_switch_door_sensor.ino
@@ -0,0 +1,30 @@
+//
+// +3.3V <> sensor <> 10K Ohm Resistor & Digitial Pin  
+// 10K Ohm Resistor <> Ground
+
+
+// this constant won't change:
+const int  buttonPin = 2;    // the pin that the pushbutton is attached to
+int buttonState;
+
+void setup() {
+  // initialize the button pin as a input:
+  pinMode(buttonPin, INPUT);
+  // initialize serial communication:
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // read the pushbutton input pin:
+  buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH) {
+      Serial.println("on");
+    } 
+    else {
+      // if the current state is LOW then the button
+      // wend from on to off:
+      Serial.println("off"); 
+    }
+  }
+  
