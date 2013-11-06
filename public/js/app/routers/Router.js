f431028669dca8d6b54dd7089f372cbe4e212c00
Ott, Mark <mark.ott@pearson.com>
2013-11-05 22:06:03 -0600

    setting up basic template of site

--- public/js/app/routers/Router.js
+++ public/js/app/routers/Router.js
@@ -1,12 +1,13 @@
 // Router.js
 
-define(["jquery", "backbone", "models/IndexModel", "views/IndexView", "collections/IndexCollection"],
+define(["jquery", "backbone", "models/IndexModel", "views/IndexView", "views/dashboardView", "collections/IndexCollection"],
 
-    function($, Backbone, Model, View, Collection) {
+    function($, Backbone, Model, View, DashboardView, Collection) {
 
-        var Router = Backbone.Router.extend({
+        // Returns the DesktopRouter class
+        return Backbone.Router.extend({
 
-            initialize: function() {
+            initialize: function () {
 
                 // Tells Backbone to start watching for hashchange events
                 Backbone.history.start();
@@ -17,21 +18,23 @@ define(["jquery", "backbone", "models/IndexModel", "views/IndexView", "collectio
             routes: {
 
                 // When there is no hash on the url, the home method is called
-                "": "index"
+                "": "getDashboard",
+                "dashboard": "getDashboard"
 
             },
 
-            index: function() {
+            index: function () {
 
                 // Instantiates a new view which will render the header text to the page
                 new View();
 
+            },
+
+            getDashboard: function () {
+                new DashboardView();
             }
 
         });
-
-        // Returns the DesktopRouter class
-        return Router;
 
     }
 
---
+++ server/config/config-template.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Mark on 11/5/13.
+ */
+
+
+exports.config = {
+    listenPort: "1337",
+    sessionSecret: "keyboard-cat",
+    database: {
+        IP: "192.168.0.10",
+        name: "arduino-home",
+        port: "27017"
+    }
+};
