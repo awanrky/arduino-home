4c1457f5bf58b0293e92b5c3c1a1603a8bb95999
Mark Ott <mark.ott@live.com>
2014-02-02 22:18:05 -0600

    cd5 sensor added on indoor_environment

--- arduino/Sketches/indoor_environment/indoor_environment.ino
+++ arduino/Sketches/indoor_environment/indoor_environment.ino
@@ -4,20 +4,25 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085.h>
 #include <aisa_BMP180.h>
+#include <aisa_DHT.h>
 #include <aisa_ethernet.h>
 #include <string.h>
 
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0xF6, 0x8E};
 
 IPAddress server(192,168,13,14);
-
-//EthernetClient client;
+int serverPort = 1337;
 
 aisa_ethernet * ethernet;
 
-aisa_BMP180 * bmp;
+char parameters[110];
 
-const String arduinoName = "indoor_environment";
+//char * dhtJson = "{\"sensorName\": \"%s\", \"degreesCelcius\": %s, \"humidity\": %s}";
+
+aisa_BMP180 * bmp;
+//aisa_DHT * dht;
+
+const char * arduinoName = "living-room";
 
 void setup()
 {
@@ -36,21 +41,95 @@ void setup()
     delay(1000);
 
     bmp = new aisa_BMP180(arduinoName);
+//    dht = new aisa_DHT(arduinoName, 2);
 
     Serial.println(F("done"));
 }
 
 void loop()
 {
+    Serial.println((int)getFreeRam);
+//    takeDhtReading();
     takeBmp180Reading();
+    takeCd5Reading();
 
-    delay(60000);
+    delay(10000);
 }
 
+int getFreeRam(void)
+{
+  extern int  __bss_end;
+  extern int  *__brkval;
+  int free_memory;
+  if((int)__brkval == 0) {
+    free_memory = ((int)&free_memory) - ((int)&__bss_end);
+  }
+  else {
+    free_memory = ((int)&free_memory) - ((int)__brkval);
+  }
+
+  return free_memory;
+}
+
+void takeCd5Reading()
+{
+    Serial.print("Cd5");
+
+    int reading = analogRead(3);
+
+    sprintf(parameters,
+        "{\"sensorName\": \"living-room\", \"reading\": %d}",
+        reading);
+
+    Serial.println(parameters);
+
+    if (ethernet->post(server, serverPort, "/api/v1/arduino-home/cd5", parameters))
+    {
+        Serial.println(" Created");
+    }
+    else
+    {
+        Serial.println(" unable to post");
+    }
+}
+
+//void takeDhtReading()
+//{
+////    char parameters[110];
+//
+//    Serial.print("DHT:");
+////
+////    dht->getReading();
+//    float degreesCelcius = dht->getTemperature(false);
+//    float humidity = dht->getHumidity(false);
+////
+//    char degreesCelciusBuffer[15];
+//    char humidityBuffer[15];
+////
+//    dtostrf(degreesCelcius, 1, 2, degreesCelciusBuffer);
+//    dtostrf(humidity, 1, 2, humidityBuffer);
+////
+////    sprintf(parameters,
+////        dhtJson,
+////        arduinoName,
+////        degreesCelciusBuffer,
+////        humidityBuffer);
+////
+//    Serial.println(parameters);
+//
+//    if (ethernet->post(server, serverPort, "/api/v1/arduino-home/dht", parameters))
+//    {
+//        Serial.println(" Created");
+//    }
+//    else
+//    {
+//        Serial.println(" unable to post");
+//    }
+//}
 
 void takeBmp180Reading()
 {
-    char parameters[110];
+//    char parameters[110];
 
     Serial.print(F("BMP180:"));
 
@@ -66,18 +145,20 @@ void takeBmp180Reading()
     dtostrf(hectoPascals, 1, 2, hectoPascalsBuffer);
     dtostrf(altitudeInMeters, 1, 2, altitudeInMetersBuffer);
 
-    int contentLength = sprintf(parameters,
+    sprintf(parameters,
         "{\"sensorName\": \"outside-deck\", \"degreesCelcius\": %s, \"hectoPascals\": %s, \"altitudeInMeters\": %s}",
         degreesCelciusBuffer,
         hectoPascalsBuffer,
         altitudeInMetersBuffer);
 
-    if (ethernet->post(server, 1337, "/api/v1/arduino-home/bmp180", parameters))
+    Serial.println(parameters);
+
+    if (ethernet->post(server, serverPort, "/api/v1/arduino-home/bmp180", parameters))
     {
         Serial.println(F(" Created"));
     }
     else
     {
-        Serial.println(F(" error"));
+        Serial.println(F(" unable to post"));
     }
 }
